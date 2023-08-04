4ec14c367396073cc59d446dc7837dfaa94602bf
myrinja <myrinjau98@gmail.com>
2023-08-04 22:05:42 +0300

    correct bugs

--- index.js
+++ index.js
@@ -1,63 +1,67 @@
 
-let div = document.querySelector('#listy');
-let ul = document.createElement('ul');
-ul.id = "movies";
-div.appendChild(ul);
 
-let base_url =  "http://localhost:3000/films";
+document.addEventListener("DOMContentLoaded", function() {
+  let div = document.querySelector('#listy');
+  let ul = document.createElement('ul');
+  ul.id = "movies";
+  div.appendChild(ul);
 
-function buyTicketBtn(event) {
-  event.stopPropagation(); // Prevent the click event from bubbling up to the li element
-  const selectedMovie = event.currentTarget.dataset.movie;
-  const movieElement = document.querySelector(`[data-movie="${selectedMovie}"]`);
-  const ticketsSold = parseInt(movieElement.dataset.ticketsSold);
-  const capacity = parseInt(movieElement.dataset.capacity);
+  let base_url = "http://localhost:3000/films";
 
-  if (ticketsSold === capacity) {
-    return;
+  function buyTicketBtn(event) {
+    const selectedButton = event.target; 
+    const li = selectedButton.closest('li'); 
+
+    if (!li) {
+      return; 
+    }
+
+    const selectedMovie = li.dataset.movie;
+    const ticketsSold = parseInt(li.dataset.ticketsSold);
+    const capacity = parseInt(li.dataset.capacity);
+
+    if (ticketsSold === capacity) {
+      alert("ticket bought successfully");
+      return;
+    }
+
+    // Update the tickets sold for the selected movie and the displayed available tickets
+    li.dataset.ticketsSold = ticketsSold + 1;
+    const availableTickets = capacity - (ticketsSold + 1);
+    li.querySelector('.tickets-available').innerText = `Tickets available: ${availableTickets}`;
   }
 
-  // Update the tickets sold for the selected movie and the displayed available tickets
-  movieElement.dataset.ticketsSold = ticketsSold + 1;
-  const availableTickets = capacity - (ticketsSold + 1);
-  movieElement.querySelector('.tickets-available').innerText = `Tickets available: ${availableTickets}`;
-}
+  fetch(base_url)
+    .then(res => res.json())
+    .then(data => {
+      data.forEach(film => {
+        let li = document.createElement('li'); // Create a new <li> element for each film
+        li.dataset.movie = film.title;
+        li.dataset.ticketsSold = film.tickets_sold;
+        li.dataset.capacity = film.capacity;
 
-fetch(base_url)
-  .then(res => res.json())
-  .then(data => {
-    data.forEach(film => {
-      let li = document.createElement('li'); // Create a new <li> element for each film
-      li.dataset.movie = film.title;
-      li.dataset.ticketsSold = film.tickets_sold;
-      li.dataset.capacity = film.capacity;
-
-      let btn = document.createElement('button');
-      btn.innerText = "Buy Ticket";
-
-      const availableTickets = film.capacity - film.tickets_sold;  
-      li.innerHTML = `
-        <p>
-          <h3> Movie title: ${film.title}</h3>
-          <h3> movie runtime: ${film.runtime} minutes</h3>
-          <h3> capacity: ${film.capacity}</h3>
-          <h3> Showtime: ${film.showtime}</h3>
-          <h3> Ticket sold: ${film.tickets_sold}</h3>
-          <p> The description of the movie: ${film.description}</p>
-          <h2 class="tickets-available">Tickets available: ${availableTickets}</h2>
-          <img src='${film.poster}'>
-        </p>
-      `;
-      ul.appendChild(li); // Append the <li> to the <ul>
-      li.appendChild(btn);
-
-      // Attach the event listener only to the "Buy Ticket" button
-      btn.addEventListener('click', buyTicketBtn);
+        const availableTickets = film.capacity - film.tickets_sold;  
+        li.innerHTML = `
+          <p>
+            <h3> Movie title: ${film.title}</h3>
+            <h3> movie runtime: ${film.runtime} minutes</h3>
+            <h3> capacity: ${film.capacity}</h3>
+            <h3> Showtime: ${film.showtime}</h3>
+            <h3> Ticket sold: ${film.tickets_sold}</h3>
+            <p> The description of the movie: ${film.description}</p>
+            <h2 class="tickets-available">Tickets available: ${availableTickets}</h2>
+            <img src='${film.poster}'>
+            <button class="buy-ticket-btn">Buy Ticket</button>
+          </p>
+        `;
+        ul.appendChild(li); // Append the <li> to the <ul>
+        
+        // Attach the event listener to the button within the li
+        const btn = li.querySelector(".buy-ticket-btn");
+        btn.addEventListener('click', buyTicketBtn);
+      });
     });
-  });
-
-
-
+});
 
 
 
